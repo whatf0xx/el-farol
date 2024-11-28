69d0985a7eef5016ba0ac058cddd9878e5a2ff8f
whatf0xx <whatf0xx@gmail.com>
2024-11-28 17:01:41 +0100

    Moved the simulation into a class so that I can change the params more easily

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
-from typing import List
 import matplotlib.pyplot as plt
-import numpy as np
-from random import random
-import networkx as nx
+from simulation import Simulation
 
 
 NIGHTS = 800
@@ -16,77 +13,18 @@ ATTENDANCE_SENSITIVITY = 0.5
 OPINION_RECOVERY = 0.21
 
 
-class Guest:
-    def __init__(
-        self,
-        id: int,
-        neighbours: List["Guest"] | None = None,
-        starting_opinion: float | None = None,
-    ):
-        self.id = id
-        self.neighbours = neighbours if neighbours is not None else []
-        self.opinion = (
-            starting_opinion
-            if starting_opinion is not None
-            else 2 * (random() - 0.5)
-        )
-
-    def __repr__(self) -> str:
-        return f"Guest{self.id}"
-
-    def evaluate_evening(self, attendance: int):
-        x = BAD_EVENING_MULTIPLIER
-        c = MAX_CAPACITY
-        s = ATTENDANCE_SENSITIVITY
-        a = attendance
-        self.opinion = x / (1 + np.exp((a - c) * s)) - x + 1
-
-    def decide(self) -> bool:
-        neighbour_opinion = sum(n.opinion for n in self.neighbours)
-        decision = self.opinion + PEER_PRESSURE * neighbour_opinion
-        if decision > 0:
-            return True
-        else:
-            self.opinion += OPINION_RECOVERY
-            return False
-
-
-def initialise_guests(n: int, k: int) -> List[Guest]:
-    graph = nx.random_regular_graph(k, n)
-    guests = {node: Guest(id=str(node)) for node in graph.nodes()}
-    # nx.draw(
-    #     graph,
-    #     with_labels=True,
-    #     node_color="lightblue",
-    #     edge_color="gray",
-    #     node_size=500,
-    # )
-    # plt.show()
-    for node1, node2 in graph.edges():
-        guests[node1].neighbours.append(guests[node2])
-        guests[node2].neighbours.append(guests[node1])
-
-    return list(guests.values())
-
-
-def night(guests: List[Guest]) -> int:
-    # print(f"{guests[50]}; opinion: {guests[50].opinion}")
-    # for n in guests[50].neighbours:
-    #     print(f"\t{n}; opinion: {n.opinion}")
-    attendees = [guest for guest in guests if guest.decide()]
-    attendance = len(attendees)
-    for guest in attendees:
-        guest.evaluate_evening(attendance)
-
-    happiness = sum(guest.opinion for guest in guests)
-
-    return attendance, happiness
-
-
 if __name__ == "__main__":
-    guests = initialise_guests(NO_GUESTS, NO_CONNECTIONS)
+    sim = Simulation(
+        NO_GUESTS,
+        NO_CONNECTIONS,
+        BAD_EVENING_MULTIPLIER,
+        MAX_CAPACITY,
+        ATTENDANCE_SENSITIVITY,
+        PEER_PRESSURE,
+        OPINION_RECOVERY,
+    )
 
-    attendance, group_happiness = zip(*[night(guests) for _ in range(NIGHTS)])
+    attendance, group_happiness = zip(*[sim.step() for _ in range(NIGHTS)])
 
     plt.figure()
     plt.plot(attendance[-SHOW_LAST:])
---
+++ simulation.py
@@ -0,0 +1,88 @@
+from typing import List
+from random import random
+import numpy as np
+import networkx as nx
+
+
+class Guest:
+    def __init__(
+        self,
+        id: int,
+        neighbours: List["Guest"] | None = None,
+        starting_opinion: float | None = None,
+    ):
+        self.id = id
+        self.neighbours = neighbours if neighbours is not None else []
+        self.opinion = (
+            starting_opinion
+            if starting_opinion is not None
+            else 2 * (random() - 0.5)
+        )
+
+    def __repr__(self) -> str:
+        return f"Guest{self.id}"
+
+    def evaluate_evening(
+        self,
+        attendance: int,
+        bad_evening_multiplier: float,
+        max_capacity: int,
+        attendance_sensitivity: float,
+    ):
+        x = bad_evening_multiplier
+        c = max_capacity
+        s = attendance_sensitivity
+        a = attendance
+        self.opinion = x / (1 + np.exp((a - c) * s)) - x + 1
+
+    def decide(self, peer_pressure: float, opinion_recovery: float) -> bool:
+        neighbour_opinion = sum(n.opinion for n in self.neighbours)
+        decision = self.opinion + peer_pressure * neighbour_opinion
+        if decision > 0:
+            return True
+        else:
+            self.opinion += opinion_recovery
+            return False
+
+
+class Simulation:
+    def __init__(
+        self,
+        n: int,
+        k: int,
+        bad_evening_multiplier: float,
+        max_capacity: int,
+        attendance_sensitivity: float,
+        peer_pressure: float,
+        opinion_recovery: float,
+    ):
+        graph = nx.random_regular_graph(k, n)
+        guests = {node: Guest(id=str(node)) for node in graph.nodes()}
+
+        for node1, node2 in graph.edges():
+            guests[node1].neighbours.append(guests[node2])
+            guests[node2].neighbours.append(guests[node1])
+
+        self.bad_evening_multiplier = bad_evening_multiplier
+        self.max_capacity = max_capacity
+        self.attendance_sensitivity = attendance_sensitivity
+        self.peer_pressure = peer_pressure
+        self.opinion_recovery = opinion_recovery
+        self.guests = list(guests.values())
+
+    def step(self) -> int:
+        p, r = self.peer_pressure, self.opinion_recovery
+        attendees = [guest for guest in self.guests if guest.decide(p, r)]
+        attendance = len(attendees)
+
+        b, m, a = (
+            self.bad_evening_multiplier,
+            self.max_capacity,
+            self.attendance_sensitivity,
+        )
+        for guest in attendees:
+            guest.evaluate_evening(attendance, b, m, a)
+
+        happiness = sum(guest.opinion for guest in self.guests)
+
+        return attendance, happiness
