4e01ef2c0d68d95e4ddddd730bf72be588b4d67a
whatf0xx <whatf0xx@gmail.com>
2024-11-28 21:14:33 +0100

    With opinion that ends slightly positive eventually

--- run.py
+++ run.py
@@ -1,32 +1,62 @@
 import matplotlib.pyplot as plt
 from simulation import Simulation
+from tqdm.contrib.itertools import product
 import numpy as np
 
 
-NIGHTS = 800
+NIGHTS = 400
 SHOW_LAST = 100
 NO_GUESTS = 300
 NO_CONNECTIONS = 11
 MAX_CAPACITY = 60
-PEER_PRESSURE = 1.2
-BAD_EVENING_MULTIPLIER = 4.0
+PEER_PRESSURE = 1.18
+BAD_EVENING_MULTIPLIER = 1.16
 ATTENDANCE_SENSITIVITY = 0.5
-OPINION_RECOVERY = 0.21
+OPINION_RECOVERY = 0.48
 
 
 if __name__ == "__main__":
-    # peer_pressures = np.linspace()
+    peer_pressures = np.linspace(0.8, 1.4, 20)
+    bad_eve_multipliers = np.linspace(1.0, 4.0, 20)
+    opinion_recoveries = np.linspace(0.05, 0.5, 20)
+    best_score = -np.inf
+    params = (None, None, None)
+    for p, b, r in product(
+        peer_pressures, bad_eve_multipliers, opinion_recoveries
+    ):
+        sim = Simulation(
+            NO_GUESTS,
+            NO_CONNECTIONS,
+            b,
+            MAX_CAPACITY,
+            ATTENDANCE_SENSITIVITY,
+            p,
+            r,
+        )
+
+        attendance, group_happiness = zip(*[sim.step() for _ in range(NIGHTS)])
+        score = sum(group_happiness[-SHOW_LAST:])
+        if score > best_score:
+            best_score = score
+            params = (p, b, r)
+
+    p, b, r = params
+    print(
+        f"PEER_PRESSURE = {p}, BAD_EVENING_MULTIPLIER = {b}, OPINION_RECOVERY = {r}"
+    )
+
     sim = Simulation(
         NO_GUESTS,
         NO_CONNECTIONS,
-        BAD_EVENING_MULTIPLIER,
+        b,
         MAX_CAPACITY,
         ATTENDANCE_SENSITIVITY,
-        PEER_PRESSURE,
-        OPINION_RECOVERY,
+        p,
+        r,
     )
 
     attendance, group_happiness = zip(*[sim.step() for _ in range(NIGHTS)])
+    score = sum(group_happiness[-SHOW_LAST:])
 
     plt.figure()
     plt.plot(attendance[-SHOW_LAST:])
